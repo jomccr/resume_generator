dc54710fd2a022377e83e0767b9b96207bb50425
John McCrary <johnmccrarywork@gmail.com>
2020-12-07 10:38:02 -0600

    Add a cachebuster at the bottom of the page

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import yaml
 
 from flask import Flask
 from flask.templating import render_template
+from time import time
+from hashlib import md5
 
 app = Flask(__name__)
 
@@ -13,6 +15,7 @@ app.template_folder = './templates/'
 def parse_yaml(filename='./static/my_resume.yaml'):
     with open(filename) as file:
         cv = yaml.full_load(file)
+        cv['cachebuster'] = lambda: md5(str(time())).hexdigest()
     return cv
 
 
@@ -20,5 +23,6 @@ def parse_yaml(filename='./static/my_resume.yaml'):
 def main():
     return render_template("main.html", content=parse_yaml())
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
